53a53749916f7d41dad71ef0b279569b77aae2f9
atonement93 <85914639+atonement93@users.noreply.github.com>
2021-09-28 17:50:40 +0900

    9/28 Crawling
    9/28 Crawling

---
+++ src/main/java/com/nj/pa/crawling/Crawler.java
@@ -0,0 +1,23 @@
+package com.nj.pa.crawling;
+
+import java.sql.Connection;
+
+import javax.swing.text.Document;
+
+import org.jsoup.Jsoup;
+
+public class Crawler {
+
+	public static void main(String[] args) {
+		
+		String URL = "https://mcountdown.genie.co.kr/vote/pre";
+		
+		Document doc = null;
+		
+
+		
+		
+
+	}
+
+}
