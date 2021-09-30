7cff31705fdfb8d9666561649bf6ef04d544980d
atonement93 <85914639+atonement93@users.noreply.github.com>
2021-09-30 12:14:29 +0900

    9/30 사전투표
    9/30 사전투표

--- src/main/webapp/resources/js/vote_pre.js
+++ src/main/webapp/resources/js/vote_pre.js
@@ -2,11 +2,3 @@
  * 
  */
  
- $(document).ready(function(){ 
-    $('selector').css('width', $(window).width()); 
-    $('selector').css('height', $(window).height()); 
-    $(window).resize(function() { 
-        $('selector').css('width', $(window).width()); 
-        $('selector').css('height', $(window).height()); 
-    }); 
-});
